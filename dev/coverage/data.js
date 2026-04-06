39be390afdf5ba962cb870cae1d8edee11e8f8cf
github-action-benchmark <github@users.noreply.github.com>
2026-04-06 18:21:52 +0000

    add Fallow Coverage (customBiggerIsBetter) benchmark result for 4bdb26a6ae0c90b99945a56f95f70f88def04245

--- dev/coverage/data.js
+++ dev/coverage/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775499415492,
+  "lastUpdate": 1775499712409,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Coverage": [
@@ -6403,6 +6403,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/fallow-rs/fallow/commit/da2dc07b4718a9331880bea23705c3b593eea056"
         },
         "date": 1775499414614,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Code Coverage",
+            "value": 93.3,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "4bdb26a6ae0c90b99945a56f95f70f88def04245",
+          "message": "fix: bump CACHE_VERSION to 20 for SFC template extraction changes",
+          "timestamp": "2026-04-06T20:20:41+02:00",
+          "tree_id": "1470ce28f39fc28ed720ba0d3bb594643e63d462",
+          "url": "https://github.com/fallow-rs/fallow/commit/4bdb26a6ae0c90b99945a56f95f70f88def04245"
+        },
+        "date": 1775499711732,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
