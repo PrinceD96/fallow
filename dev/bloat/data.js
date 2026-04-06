b89f0493ccd2a98d7dc3845f63a57776445fe972
github-action-benchmark <github@users.noreply.github.com>
2026-04-06 18:18:59 +0000

    add Fallow Binary Size (customSmallerIsBetter) benchmark result for da2dc07b4718a9331880bea23705c3b593eea056

--- dev/bloat/data.js
+++ dev/bloat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775499065187,
+  "lastUpdate": 1775499539498,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Binary Size": [
@@ -4088,6 +4088,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary Size (fallow)",
             "value": 144293544,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bartwaardenburg@gmail.com",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "da2dc07b4718a9331880bea23705c3b593eea056",
+          "message": "Merge pull request #58 from M-Hassan-Raza/feat/sfc-template-usage\n\nfeat: track template-visible import usage in SFCs",
+          "timestamp": "2026-04-06T20:15:42+02:00",
+          "tree_id": "bc7e6477f60ffabd9b94c0c528b4f7cc1687a41f",
+          "url": "https://github.com/fallow-rs/fallow/commit/da2dc07b4718a9331880bea23705c3b593eea056"
+        },
+        "date": 1775499538189,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size (fallow)",
+            "value": 145425744,
             "unit": "bytes"
           }
         ]
