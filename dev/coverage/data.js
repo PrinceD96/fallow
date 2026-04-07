e7c663031b64ba49ffe472fbc0282a8a3c23f46c
github-action-benchmark <github@users.noreply.github.com>
2026-04-07 22:10:22 +0000

    add Fallow Coverage (customBiggerIsBetter) benchmark result for 621225af0cf364089b9b1684068b9ec7a4c5f794

--- dev/coverage/data.js
+++ dev/coverage/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775599645522,
+  "lastUpdate": 1775599822277,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Coverage": [
@@ -8085,6 +8085,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/fallow-rs/fallow/commit/e1fed16c3ae8e3de70fd2bfcd922b8a81f1c60c2"
         },
         "date": 1775599643901,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Code Coverage",
+            "value": 94.1,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "621225af0cf364089b9b1684068b9ec7a4c5f794",
+          "message": "chore: release v2.19.2",
+          "timestamp": "2026-04-08T00:08:56+02:00",
+          "tree_id": "0b517965cd9c591cea4a3b699f166993359167f3",
+          "url": "https://github.com/fallow-rs/fallow/commit/621225af0cf364089b9b1684068b9ec7a4c5f794"
+        },
+        "date": 1775599820677,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
