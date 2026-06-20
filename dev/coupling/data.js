6524cee31943cf691db1519856904fe863868060
github-action-benchmark <github@users.noreply.github.com>
2026-06-20 13:42:27 +0000

    add Module Coupling (customSmallerIsBetter) benchmark result for 83096135fb6d43f6aafe6979061406cdbb7da62d

--- dev/coupling/data.js
+++ dev/coupling/data.js
@@ -1,57 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781962654469,
+  "lastUpdate": 1781962947128,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Module Coupling": [
-      {
-        "commit": {
-          "author": {
-            "email": "bart@waardenburg.dev",
-            "name": "Bart Waardenburg",
-            "username": "BartWaardenburg"
-          },
-          "committer": {
-            "email": "bart@waardenburg.dev",
-            "name": "Bart Waardenburg",
-            "username": "BartWaardenburg"
-          },
-          "distinct": true,
-          "id": "88519c6c4bd828c2af44fc95adb55acd1e436c0a",
-          "message": "refactor: group sfc template inputs",
-          "timestamp": "2026-06-20T10:28:06+02:00",
-          "tree_id": "c4deef171007c1ce480b12b6af3ed89644628fa9",
-          "url": "https://github.com/fallow-rs/fallow/commit/88519c6c4bd828c2af44fc95adb55acd1e436c0a"
-        },
-        "date": 1781944219356,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Max Fan-In (non-framework)",
-            "value": 27,
-            "unit": "deps"
-          },
-          {
-            "name": "Max Fan-Out (non-framework)",
-            "value": 28,
-            "unit": "deps"
-          },
-          {
-            "name": "Modules >20 Fan-In (%)",
-            "value": 1.01,
-            "unit": "%"
-          },
-          {
-            "name": "Total Modules",
-            "value": 398,
-            "unit": "count"
-          },
-          {
-            "name": "Total Edges",
-            "value": 991,
-            "unit": "count"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -4874,6 +4825,55 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/fallow-rs/fallow/commit/2e7473e679250855cc4827a1e2144a91e4d30735"
         },
         "date": 1781962651835,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Max Fan-In (non-framework)",
+            "value": 27,
+            "unit": "deps"
+          },
+          {
+            "name": "Max Fan-Out (non-framework)",
+            "value": 28,
+            "unit": "deps"
+          },
+          {
+            "name": "Modules >20 Fan-In (%)",
+            "value": 1.01,
+            "unit": "%"
+          },
+          {
+            "name": "Total Modules",
+            "value": 398,
+            "unit": "count"
+          },
+          {
+            "name": "Total Edges",
+            "value": 991,
+            "unit": "count"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "83096135fb6d43f6aafe6979061406cdbb7da62d",
+          "message": "refactor: group framework finding population",
+          "timestamp": "2026-06-20T15:41:01+02:00",
+          "tree_id": "cdeb27705e596f8ac8c4869f6c3aefee420a33f9",
+          "url": "https://github.com/fallow-rs/fallow/commit/83096135fb6d43f6aafe6979061406cdbb7da62d"
+        },
+        "date": 1781962944013,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
