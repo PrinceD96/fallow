54083f99e8889815f4b78b2bc502b2d0d87515be
github-action-benchmark <github@users.noreply.github.com>
2026-08-02 08:18:12 +0000

    add Fallow Conformance (customBiggerIsBetter) benchmark result for 763066883ef36e51c0c99c9971b676beca9a8e55

--- dev/conformance/data.js
+++ dev/conformance/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785572109375,
+  "lastUpdate": 1785658692960,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Conformance": [
@@ -7332,6 +7332,88 @@ window.BENCHMARK_DATA = {
           {
             "name": "Fallow Total",
             "value": 30173,
+            "unit": "issues"
+          },
+          {
+            "name": "Knip Total",
+            "value": 2030,
+            "unit": "issues"
+          },
+          {
+            "name": "fastify Agreement",
+            "value": 6.1,
+            "unit": "%"
+          },
+          {
+            "name": "next.js Agreement",
+            "value": 1.7,
+            "unit": "%"
+          },
+          {
+            "name": "preact Agreement",
+            "value": 4.3,
+            "unit": "%"
+          },
+          {
+            "name": "query Agreement",
+            "value": 0,
+            "unit": "%"
+          },
+          {
+            "name": "svelte Agreement",
+            "value": 0.3,
+            "unit": "%"
+          },
+          {
+            "name": "vite Agreement",
+            "value": 5.8,
+            "unit": "%"
+          },
+          {
+            "name": "vue-core Agreement",
+            "value": 22.8,
+            "unit": "%"
+          },
+          {
+            "name": "zod Agreement",
+            "value": 2.8,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg",
+            "email": "bart@waardenburg.dev"
+          },
+          "committer": {
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg",
+            "email": "bart@waardenburg.dev"
+          },
+          "id": "763066883ef36e51c0c99c9971b676beca9a8e55",
+          "message": "docs: use dead-code instead of the deprecated check alias in the migration doc",
+          "timestamp": "2026-08-02T06:28:57Z",
+          "url": "https://github.com/fallow-rs/fallow/commit/763066883ef36e51c0c99c9971b676beca9a8e55"
+        },
+        "date": 1785658689866,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Agreement Rate",
+            "value": 1.8,
+            "unit": "%"
+          },
+          {
+            "name": "Agreed Issues",
+            "value": 576,
+            "unit": "issues"
+          },
+          {
+            "name": "Fallow Total",
+            "value": 30176,
             "unit": "issues"
           },
           {
